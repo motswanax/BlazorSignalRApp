57f3134f734d739ad09351ba6ea6988f7fbf7bc7
Baiketli Dijo <baiketlid@gmail.com>
2024-11-10 09:12:49 +0200

    Add project files.

---
+++ BlazorSignalRApp/Program.cs
@@ -0,0 +1,53 @@
+using BlazorSignalRApp.Client.Pages;
+using BlazorSignalRApp.Components;
+using BlazorSignalRApp.Hubs;
+
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSignalR();
+
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        ["application/octet-stream"]);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseResponseCompression();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorSignalRApp.Client._Imports).Assembly);
+
+app.UseResponseCompression();
+
+app.MapHub<ChatHub>("/chathub");
+
+app.Run();
